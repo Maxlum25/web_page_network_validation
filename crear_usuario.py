c0f5fb0178c66844ebb20e2728969452f9d0a75c
Maxlum25 <alfredolubies@gmail.com>
2026-09-02 20:06:27 -0400

    segundo

--- crear_usuario.py
+++ crear_usuario.py
@@ -1,4 +1,45 @@
 import sqlite3
+import os
+from dotenv import load_dotenv
+import bcrypt
 
-username=""
-password = ""
+
+
+def main():
+    while True:
+        username=input("Ingresa el usuario: ")
+        password = input(f"Ingresa la contraseña de {username}: ")
+        confirm = input(f"¿Estos datos son correctos? {username} / {password} 's/n': ")
+        if confirm.lower() == "s":
+            break
+    
+    password_bcode = password.encode("utf-8")
+    hashed = bcrypt.hashpw(password_bcode, bcrypt.gensalt())
+    load_dotenv()
+
+    DATABASE = os.getenv("DATABASE")
+    
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    
+    cur.execute("""
+        CREATE TABLE users(
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL )
+    """)
+    
+    cur.execute("""
+       INSERT INTO users (username, password) VALUES (?, ?)
+            """, (username, hashed))
+    
+    con.commit()
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    
+    main()
+
